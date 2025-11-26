97f8b8dd6ab1697e87015fbba55d0e4b150289ed
caleb-kipp <190889275+caleb-kipp@users.noreply.github.com>
2025-11-26 07:23:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,49 @@
-// This is the "Offline page" service worker
+const CACHE_NAME = "estore-cache-v1";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const ASSETS_TO_CACHE = [
+  "/Estore/",
+  "/Estore/index.html",
+  "/Estore/styles.css",
+  "/Estore/script.js",
+  "/Estore/manifest.json",
+  "/Estore/icons/icon-72.png",
+  "/Estore/icons/icon-96.png",
+  "/Estore/icons/icon-128.png",
+  "/Estore/icons/icon-144.png",
+  "/Estore/icons/icon-152.png",
+  "/Estore/icons/icon-192.png",
+  "/Estore/icons/icon-384.png",
+  "/Estore/icons/icon-512.png"
+];
 
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener('install', async (event) => {
+// Activate SW
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => 
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch logic
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match("/Estore/index.html"))
+      );
+    })
+  );
+});
